a5b33e8b3be886e0bf907a6393dee12a33dd5ed7
Kinkandju <ferretsphilosophy@gmail.com>
2024-01-11 14:05:19 +0300

    feat: add function getAverage

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -148,8 +148,20 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+
+/* the reduce() method allows us to perform an operation on 
+the elements of the array with the accumulation of the result */
+/* the toFixed() method rounds the number to the specified number of decimal places */
+/* (total, num) => total + num - callback function */
+function getAverage(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  const sum = arr.reduce((total, num) => total + num, 0);
+  const average = sum / arr.length;
+
+  return Number(average.toFixed(2));
 }
 
 /**
